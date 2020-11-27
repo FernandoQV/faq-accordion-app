9d7bbf85c449fbe09a5be7969a1ed55c405b6d1c
FernandoQ <octavius.sys@gmail.com>
2020-11-26 20:53:22 -0500

    arrow corregido

--- src/card/Card.jsx
+++ src/card/Card.jsx
@@ -1,31 +1,32 @@
 import React, { useState } from "react";
+import styled from 'styled-components'
 import { Image, Title2, Paragraph, Linea } from "../atoms/Atoms";
 import { WpFaq, WpQuestion } from "../molecules/Molecules";
 import Arrow from "../images/icon-arrow-down.svg";
+const Arroww=styled(Image)`
+transform:${props=>props.rotate?'rotate(180deg)':'rotate(0deg)'};
+`;
 const Card = (props) => {
   const {question1,response} =props.faq
 
   /* esatdo oontrolador de arrow */
-  const [activeQuestion, setActiveQuestion] = useState(true);
+  const [activeQuestion, setActiveQuestion] = useState(false);
 
   /* estado del parrafo respuesta */
 
-  const [hiddenParagraph, setHiddenParagraph] = useState(false);
+  const [hiddenParagraph, setHiddenParagraph] = useState(true);
 
   /* funcion que se activa al clickear la arrow */
-  const activeFaq = () => {
-    const arrow = document.getElementById("arrow");
-    activeQuestion
-      ? (arrow.style.transform = "rotate(180deg)")
-      : (arrow.style.transform = "rotate(360deg)");
-    setActiveQuestion(!activeQuestion);
-    setHiddenParagraph(!hiddenParagraph);
+  
+  const activeFaq = (event) => {
+    setActiveQuestion(!activeQuestion)
+    setHiddenParagraph(!hiddenParagraph)
   };
   return (
     <WpFaq>
       <WpQuestion>
         <Title2>{question1}</Title2>
-        <Image src={Arrow} id="arrow" onClick={() => activeFaq()} />
+        <Arroww src={Arrow} rotate={activeQuestion} onClick={(event) => activeFaq(event)} />
       </WpQuestion>
       <Paragraph hidden={hiddenParagraph}>{response}</Paragraph>
       <Linea />
